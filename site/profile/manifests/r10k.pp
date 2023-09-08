b7b92e51bf09ecd77338f0e9b010c434e0aef86d
caslatcmu <98353190+caslatcmu@users.noreply.github.com>
2023-09-08 18:15:36 -0400

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,7 +2,7 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/caslatcmu/puppet-essential-control-repo',
   }
-  class {'r10k::webook':
+  class {'r10k::webhook':
     user  => 'root',
     group => 'root',
   }
