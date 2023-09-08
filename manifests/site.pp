71c5cb27b0a9b9cbc837c0f9ea942139a2bf5b70
caslatcmu <98353190+caslatcmu@users.noreply.github.com>
2023-09-08 15:29:25 -0400

    Update site.pp
    added default node motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,17 @@
 node 'rhhost1.cmu-19gltq3.kvmlocal' {
   include role::master
+
+  file { '/etc/puppet-managed':
+    ensure => present,
+    mode => 0644,
+    content => "This is the puppet-master!\n",
+  }
+}
+
+node default {
+  file { '/etc/motd':
+    ensure => present,
+    mode => 0644,
+    content => "This is a puppet-managed node!\n",
+  }
 }
