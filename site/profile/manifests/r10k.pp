bf904ea07e39dcab8fd444905bce269ed05dc357
caslatcmu <98353190+caslatcmu@users.noreply.github.com>
2023-09-11 11:40:55 -0400

    Update r10k.pp
    removing webhook class def which seems to be incorrect

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,8 +2,4 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/caslatcmu/puppet-essential-control-repo',
   }
-  class {'r10k::webhook':
-    user  => 'root',
-    group => 'root',
-  }
 }
